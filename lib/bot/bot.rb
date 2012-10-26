fb320f84501955ea6deb8232cf1c56be6bf6c8cf
Danny Ruiz <ynnadrules@gmail.com>
2012-10-25 19:44:48 -0500

    added load paths

--- lib/bot/bot.rb
+++ lib/bot/bot.rb
@@ -5,19 +5,25 @@ class Bot
   extend Configuration
   include HTTParty
 
-  base_uri "http://no-limit-code-em.com/sandbox"
-
+  base_uri Configuration::TEST_ENDPOINT
 
   def initialize
 
   end
 
   def get_game_state
-    self.class.get "/players/#{PLAYER_KEY}"
+    response = self.class.get "/players/#{PLAYER_KEY}"
+
+    @game_state = JSON.parse response.body
+
+    @game_state
   end
 
-  def make_play( move )
+  def make_play( move, params = {} )
 
+    response = self.class.post "/players/#{PLAYER_KEY}/#{move}", body: params
+
+    @game_state = JSON.parse response.body
   end
 
 end
--- lib/bot/config.rb
+++ lib/bot/config.rb
@@ -1,2 +1,38 @@
 module Bot
+  module Configuration
+
+    VALID_KEYS = [
+      :api_endpoint,
+      :player_key,
+    ]
+
+    API_ENDPOINT = "http://no-limit-code-em.com/api"
+
+    PLAYER_KEY = ""
+
+    SANDBOX_PLAYER_KEY = ""
+
+    TEST_ENDPOINT = "http://no-limit-code-em.com/sandbox"
+
+    TEST_PLAYER_KEYS = {
+      bet: "",  # use this to get a game state where you must respond with a bet
+      cards: "" #use this to get a game state where you must respond with a replacement of cards
+    }
+
+    attr_accessor( *VALID_KEYS )
+
+    def configure
+      yield self
+    end
+
+    def self.extended( base ) 
+      base.set_defaults
+    end
+
+    def set_defaults
+      self.api_endpoint = API_ENDPOINT
+      self.player_key = PLAYER_KEY
+    end
+
+  end
 end
